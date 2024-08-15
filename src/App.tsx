2217c68d3571849e911538776e732b8a07fbe6b9
yosheng <sad445566@gmail.com>
2024-08-16 01:26:13 +0800

    fix: dot navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.scss';
-import ReactFullpage, {fullpageOptions} from '@fullpage/react-fullpage';
+import ReactFullpage, {fullpageOptions, fullpageApi} from '@fullpage/react-fullpage';
 import Home from "./components/Home";
 import Profile from "@/components/Profile";
 import Achievement from "@/components/Achievement";
@@ -8,11 +8,14 @@ import Experience from "@/components/Experience";
 import Interest from "@/components/Interest";
 import Future from "@/components/Future";
 import Navigation from "@/components/Navigation";
+import OverlayTrigger from 'react-bootstrap/esm/OverlayTrigger';
+import {Tooltip} from "react-bootstrap";
 
 const App = () => {
     const [activePage, setActivePage] = useState('home');
+    const [fullpageApi, setFullpageApi] = useState<fullpageApi>(); // 添加一个状态来保存 fullpageApi 实例
 
-    const options : fullpageOptions = {
+    const options: fullpageOptions = {
         licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
         sectionsColor: [
             'transparent',
@@ -47,73 +50,83 @@ const App = () => {
     };
 
     const fullpageNavList = [
-        { page: 'home', title: '个人简历' },
-        { page: 'profile', title: '基本资料' },
-        { page: 'achievement', title: '个人成就' },
-        { page: 'experience', title: '工作经历' },
-        { page: 'interest', title: '实务经验' },
-        { page: 'future', title: '展望未来' }
+        {page: 'home', title: '个人简历'},
+        {page: 'profile', title: '基本资料'},
+        {page: 'achievement', title: '个人成就'},
+        {page: 'experience', title: '工作经历'},
+        {page: 'interest', title: '实务经验'},
+        {page: 'future', title: '展望未来'}
     ];
 
     return (
         <div id="app">
-            <Navigation />
+            <Navigation/>
             <ReactFullpage
                 {...options}
-                render={({ state, fullpageApi }) => (
-                    <>
-                        <div className="section item-1">
-                            <div className={`page-content ${activePage === 'home' ? 'visible' : 'hidden'}`}>
-                                <Home />
+                render={({state, fullpageApi}) => {
+                    if (!fullpageApi && !state) {
+                        // 确保在初次渲染时保存 fullpageApi
+                        setFullpageApi(fullpageApi);
+                    }
+
+                    return (
+                        <>
+                            <div className="section item-1">
+                                <div className={`page-content ${activePage === 'home' ? 'visible' : 'hidden'}`}>
+                                    <Home/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="section item-2">
-                            <div className={`page-content ${activePage === 'profile' ? 'visible' : 'hidden'}`}>
-                                <Profile />
+                            <div className="section item-2">
+                                <div className={`page-content ${activePage === 'profile' ? 'visible' : 'hidden'}`}>
+                                    <Profile/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="section item-3">
-                            <div className={`page-content ${activePage === 'achievement' ? 'visible' : 'hidden'}`}>
-                                <Achievement />
+                            <div className="section item-3">
+                                <div className={`page-content ${activePage === 'achievement' ? 'visible' : 'hidden'}`}>
+                                    <Achievement/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="section item-4">
-                            <div className={`page-content ${activePage === 'experience' ? 'visible' : 'hidden'}`}>
-                                <Experience />
+                            <div className="section item-4">
+                                <div className={`page-content ${activePage === 'experience' ? 'visible' : 'hidden'}`}>
+                                    <Experience/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="section item-5">
-                            <div className={`page-content ${activePage === 'interest' ? 'visible' : 'hidden'}`}>
-                                <Interest />
+                            <div className="section item-5">
+                                <div className={`page-content ${activePage === 'interest' ? 'visible' : 'hidden'}`}>
+                                    <Interest/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="section item-6">
-                            <div className={`page-content ${activePage === 'future' ? 'visible' : 'hidden'}`}>
-                                <Future />
+                            <div className="section item-6">
+                                <div className={`page-content ${activePage === 'future' ? 'visible' : 'hidden'}`}>
+                                    <Future/>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            id="fp-nav"
-                            className="d-none d-sm-block"
-                            style={{ marginTop: '-68px', right: '17px' }}
-                        >
-                            <ul>
-                                {fullpageNavList.map((item, index) => (
-                                    <li key={index} data-toggle="tooltip" data-placement="left" title={item.title}>
-                                        <a
-                                            href={`#${item.page}`}
-                                            className={item.page === activePage ? 'active' : ''}
-                                            onClick={() => fullpageApi.moveTo(item.page)}
-                                        >
-                                            {item.title}
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </>
-                )}
+                        </>
+                    );
+                }}
             />
+            <div
+                id="nav-dot"
+                className="d-none d-sm-block"
+            >
+                <ul>
+                    {fullpageNavList.map((item, index) => (
+                        <li key={index} style={{ listStyleType: 'none' }}>
+                            <OverlayTrigger
+                                placement="left"
+                                overlay={<Tooltip>{item.title}</Tooltip>}
+                            >
+                                <a
+                                    href={`#${item.page}`}
+                                    className={item.page === activePage ? 'active' : ''}
+                                    onClick={() => fullpageApi && fullpageApi.moveTo(item.page)}
+                                >
+                                </a>
+                            </OverlayTrigger>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 };
