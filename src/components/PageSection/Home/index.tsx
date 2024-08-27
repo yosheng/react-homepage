6cfa65e024c3736eb7ffc570c35663182eebc19f
yosheng <sad445566@gmail.com>
2024-08-27 08:38:01 +0800

    feat(Home): add intro-section

--- src/components/PageSection/Home/index.tsx
+++ src/components/PageSection/Home/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import {Col, Container, Row} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faFacebook, faGithub, faLinkedin, faSlideshare, faWordpress} from '@fortawesome/free-brands-svg-icons';
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import {faCakeCandles, faChevronDown, faLocationDot, faMapLocation} from '@fortawesome/free-solid-svg-icons';
 import './style.scss';
 
 const Home: React.FC = () => {
+
+    const introData = [
+        {icon: faLocationDot, label: 'Residence', value: 'ShangHai'},
+        {icon: faMapLocation, label: 'From', value: 'Taiwan'},
+        {icon: faCakeCandles, label: 'Birthday', value: '1993.03'},
+    ];
+
     return (
         <Container className="word">
             <Row className="userPic">
@@ -23,30 +30,37 @@ const Home: React.FC = () => {
             <Col
                 xs={12}
                 className="motto resume-hide hinge-sty"
-                style={{ position: 'relative' }}
+                style={{position: 'relative'}}
             >
                 Yosheng
             </Col>
-            <Col xs={12} style={{ fontSize: '0.3em' }}>1993年 牡羊男</Col>
             <Col xs={12} className="social-links">
                 <a href="https://blog.yosheng.tw">
-                    <FontAwesomeIcon icon={faWordpress} fixedWidth />
+                    <FontAwesomeIcon icon={faWordpress} fixedWidth/>
                 </a>
                 <a href="https://www.linkedin.com/in/yosheng-zhang-020962102">
-                    <FontAwesomeIcon icon={faLinkedin} fixedWidth />
+                    <FontAwesomeIcon icon={faLinkedin} fixedWidth/>
                 </a>
                 <a href="https://github.com/yosheng">
-                    <FontAwesomeIcon icon={faGithub} fixedWidth />
+                    <FontAwesomeIcon icon={faGithub} fixedWidth/>
                 </a>
                 <a href="https://www.facebook.com/yosheng0323">
-                    <FontAwesomeIcon icon={faFacebook} fixedWidth />
+                    <FontAwesomeIcon icon={faFacebook} fixedWidth/>
                 </a>
                 <a href="https://www.slideshare.net/ssuserf01a96">
-                    <FontAwesomeIcon icon={faSlideshare} fixedWidth />
+                    <FontAwesomeIcon icon={faSlideshare} fixedWidth/>
                 </a>
             </Col>
+            <Col xs={12} className="intro-section">
+                {introData.map((item, index) => (
+                    <div key={index} className="intro-section-row">
+                        <span><FontAwesomeIcon icon={item.icon} fixedWidth style={{marginRight: 5}}/>{item.label}</span>
+                        <span>{item.value}</span>
+                    </div>
+                ))}
+            </Col>
             <button className="next-page">
-                <FontAwesomeIcon icon={faChevronDown} fixedWidth />
+                <FontAwesomeIcon icon={faChevronDown} fixedWidth/>
             </button>
         </Container>
     );
