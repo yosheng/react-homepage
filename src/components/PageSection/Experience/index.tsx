f5e22433587f5cebce0c010c1be529bb518b3ba8
yosheng <sad445566@gmail.com>
2024-08-29 16:36:54 +0800

    feat(Experience): modify content

--- src/components/PageSection/Experience/index.tsx
+++ src/components/PageSection/Experience/index.tsx
@@ -1,83 +1,88 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 import './style.scss'
 
 // 引入图片
-import shuImg from '@/assets/images/page-4/01-shu.jpg';
 import asusImg from '@/assets/images/page-4/02-asus.jpg';
 import sceImg from '@/assets/images/page-4/03-sce.jpg';
 import sweetomeImg from '@/assets/images/page-4/04-sweetome.jpg';
+import futongImg from '@/assets/images/page-4/05-futong.png';
+import flexemImg from '@/assets/images/page-4/06-flexem.png';
+import synyiImg from '@/assets/images/page-4/07-synyi.png';
+import {useTranslation} from "react-i18next";
 
 const Experience = () => {
+
+    const {t} = useTranslation();
+
+    const experiences = [
+        {
+            imgSrc: asusImg,
+            name: t('experience.work.asus.name'),
+            role: t('experience.work.asus.role'),
+            description: t('experience.work.asus.description')
+        },
+        {
+            imgSrc: sceImg,
+            name: t('experience.work.sce.name'),
+            role: t('experience.work.sce.role'),
+            description: t('experience.work.sce.description')
+        },
+        {
+            imgSrc: sweetomeImg,
+            name: t('experience.work.sweetome.name'),
+            role: t('experience.work.sweetome.role'),
+            description: t('experience.work.sweetome.description')
+        },
+        {
+            imgSrc: futongImg,
+            name: t('experience.work.futong.name'),
+            role: t('experience.work.futong.role'),
+            description: t('experience.work.futong.description')
+        },
+        {
+            imgSrc: flexemImg,
+            name: t('experience.work.flexem.name'),
+            role: t('experience.work.flexem.role'),
+            description: t('experience.work.flexem.description')
+        },
+        {
+            imgSrc: synyiImg,
+            name: t('experience.work.synyi.name'),
+            role: t('experience.work.synyi.role'),
+            description: t('experience.work.synyi.description')
+        }
+    ];
+
     return (
         <Container>
             <Row className="d-none d-md-block">
                 <Col xs={12}>
-                    <span className="title" style={{ display: 'inline-block' }}>工作經歷</span>
+                    <span className="experience-title">{t('experience.title')}</span>
                 </Col>
             </Row>
             <Row className="d-none d-md-block">
                 <Col xs={12}>
-                    <span className="sign" style={{ fontSize: '20px' }}>衷於學習 樂於分享</span>
+                    <span className="experience-sign">{t('experience.sign')}</span>
                 </Col>
             </Row>
             <Row>
-                <Col xs={6} md={3}>
-                    <div className="thumbnail">
-                        <img
-                            src={shuImg}
-                            className="rounded-circle team-img"
-                            alt="世新大學"
-                        />
-                        <div className="caption">
-                            <h3>世新大學</h3>
-                            <p>維修工讀生</p>
-                            <p>基本軟硬體維修和Linux工作站服務架設與管理</p>
+                {experiences.map((exp, index) => (
+                    <Col xs={6} md={4} key={index}>
+                        <div className="thumbnail">
+                            <img
+                                src={exp.imgSrc}
+                                className="rounded-circle team-img"
+                                alt={exp.name}
+                            />
+                            <div className="caption">
+                                <h3>{exp.name}</h3>
+                                <p>{exp.role}</p>
+                                <p>{exp.description}</p>
+                            </div>
                         </div>
-                    </div>
-                </Col>
-                <Col xs={6} md={3}>
-                    <div className="thumbnail">
-                        <img
-                            src={asusImg}
-                            className="rounded-circle team-img"
-                            alt="華碩"
-                        />
-                        <div className="caption">
-                            <h3>華碩</h3>
-                            <p>校園實習生</p>
-                            <p>市場的行銷學和產品檢測流程管理</p>
-                        </div>
-                    </div>
-                </Col>
-                <Col xs={6} md={3}>
-                    <div className="thumbnail">
-                        <img
-                            src={sceImg}
-                            className="rounded-circle team-img"
-                            alt="文化大學"
-                        />
-                        <div className="caption">
-                            <h3>文化大學</h3>
-                            <p>程式設計師</p>
-                            <p>後端程式開發和系統規劃與建置</p>
-                        </div>
-                    </div>
-                </Col>
-                <Col xs={6} md={3}>
-                    <div className="thumbnail">
-                        <img
-                            src={sweetomeImg}
-                            className="rounded-circle team-img"
-                            alt="斯维登"
-                        />
-                        <div className="caption">
-                            <h3>斯维登</h3>
-                            <p>软件开发工程师</p>
-                            <p>全栈开发和系统维护</p>
-                        </div>
-                    </div>
-                </Col>
+                    </Col>
+                ))}
             </Row>
         </Container>
     );
